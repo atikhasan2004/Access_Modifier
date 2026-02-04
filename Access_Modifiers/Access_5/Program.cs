163b776e3cc0656f1be1fe6bd074b8558a46c5e5
Atik Hasan <saatikhasan5021@gmail.com>
2026-02-04 12:28:17 +0600

    Add files via upload
    Create a access & access modifier code.

---
+++ Access_Modifiers/Access_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class BaseClass
+{
+    protected int number;
+
+}
+class DerivedClass:BaseClass
+{
+    public void SetNumber(int n)
+    {
+        number=n;
+    }
+    public void ShowNumber()
+    {
+        Console.WriteLine($"Number= {number}");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        DerivedClass DC=new DerivedClass();
+        DC.SetNumber(22);
+        DC.ShowNumber();
+    }
+}
---
+++ Access_Modifiers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Person
+{
+    public string Name;       // Public field
+    private int age;          // Private field
+    public void SetAge(int a)   // Method to set age
+    {
+        if (a >= 0)
+            age = a;
+        else
+            Console.WriteLine("Age cannot be negative");
+    }
+
+    public void ShowInfo()      // Method to show info
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + age);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Person p = new Person();
+        p.Name = "Atik Hasan";   // Public → can access directly
+        p.SetAge(21);            // Private age set through method
+        p.ShowInfo();            // Show output
+    }
+}
